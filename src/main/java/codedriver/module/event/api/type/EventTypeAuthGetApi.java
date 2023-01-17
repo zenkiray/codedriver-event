8a88e7be99a8a41843ef30fd7c5977accb4b711a
zenkiray <zenkiray@live.com>
2023-01-17 14:56:31 +0800

    test
    Signed-off-by: zenkiray <zenkiray@live.com>

--- src/main/java/codedriver/module/event/api/type/EventTypeAuthGetApi.java
+++ src/main/java/codedriver/module/event/api/type/EventTypeAuthGetApi.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright(c) 2023 TechSure Co., Ltd. All Rights Reserved.
+ * 本内容仅限于深圳市赞悦科技有限公司内部传阅，禁止外泄以及用于其他的商业项目。
+ */
+
 package codedriver.module.event.api.type;
 
 import codedriver.framework.auth.core.AuthAction;
@@ -15,6 +20,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -62,27 +68,25 @@ public class EventTypeAuthGetApi extends PrivateApiComponentBase {
         if (eventTypeVo == null) {
             throw new EventTypeNotFoundException(id);
         }
-        /** 获取当前事件类型本身的授权 */
+        /* 获取当前事件类型本身的授权 */
         List<AuthorityVo> authList = eventTypeMapper.getAuthorityByEventTypeId(id);
         EventTypeVo authVo = new EventTypeVo();
         authVo.setAuthorityVoList(authList);
         result.put("authList", authVo.getAuthorityList());
-        /** 获取子类的授权 */
+        /* 获取子类的授权 */
         List<EventTypeVo> children = eventTypeMapper.getChildrenByLeftRightCode(eventTypeVo.getLft(), eventTypeVo.getRht());
         if (CollectionUtils.isNotEmpty(children)) {
             List<Long> childrenIds = children.stream().map(EventTypeVo::getId).collect(Collectors.toList());
-            /** 不同的子类可能有相同的权限，用set去重 */
+            /* 不同的子类可能有相同的权限，用set去重 */
             Set<AuthorityVo> childrenAuthSet = new HashSet<>();
             for (Long childId : childrenIds) {
                 List<AuthorityVo> childrenAuths = eventTypeMapper.getAuthorityByEventTypeId(childId);
                 if (CollectionUtils.isNotEmpty(childrenAuths)) {
-                    for (AuthorityVo vo : childrenAuths) {
-                        childrenAuthSet.add(vo);
-                    }
+                    childrenAuthSet.addAll(childrenAuths);
                 }
             }
             if (CollectionUtils.isNotEmpty(childrenAuthSet)) {
-                List<AuthorityVo> childrenAuthList = childrenAuthSet.stream().collect(Collectors.toList());
+                List<AuthorityVo> childrenAuthList = new ArrayList<>(childrenAuthSet);
                 EventTypeVo childrenAuthVo = new EventTypeVo();
                 childrenAuthVo.setAuthorityVoList(childrenAuthList);
                 result.put("childrenAuthList", childrenAuthVo.getAuthorityList());
